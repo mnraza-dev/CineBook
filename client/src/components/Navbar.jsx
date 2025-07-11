a1051dcd72afadaa0c6c32956cb522736b560030
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 05:23:29 +0530

    fix: update Navbar component to include SearchIcon and adjust login button layout; modify primary and secondary color variables in CSS

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, SearchIcon } from "lucide-react";
 const Navbar = () => {
   return (
     <header className="sticky top-0 left-0 z-50 w-full flex px-6 md:px-16 lg:px-36 py-5 bg-primary/90">
@@ -14,10 +14,14 @@ const Navbar = () => {
           <Link to="/my-bookings">My Bookings</Link>
           <Link to="/favorite">My Favorites</Link>
         </div>
-        <div>
-          <Link to="/login" className="text-white">
+        <div className="flex items-center gap-4">
+          <SearchIcon className="w-6 h-6 max-md:hidden cursor-pointer" />
+          {/* <Link to="/login" className="text-white">
             Login
-          </Link>
+          </Link> */}
+          <button className="bg-secondary text-white px-6 py-1 cursor-pointer rounded hover:bg-primary-light/80 transition">
+           Login
+          </button>
         </div>
 
         <MenuIcon className="max-md:ml-4 w-8 h-8 cursor-pointer md:hidden" />
