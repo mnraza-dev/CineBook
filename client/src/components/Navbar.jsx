64d0045e188a49de3e841606489c241441d665b7
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 05:02:48 +0530

    refactor: remove Header component and update Navbar with routing and logo
    style: add global styles and theme variables in index.css
    
    feat: implement basic structure for Home page
    
    build: integrate Tailwind CSS into Vite configuration
    
    chore: add SVG asset for future use

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { assets } from "../assets/assets";
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
+    <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5  bg-primary/90">
+      <Link to={"/"}>
+        <img src={assets.logo} alt="Logo" className="w-24 h-auto" />
+      </Link>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/"}>Home</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
