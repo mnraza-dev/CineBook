5157f20af2174b15c7c07af3ec884ec6864727ec
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 04:21:08 +0530

    feat: Set up backend with Express, MongoDB connection, and initial model structure

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Backend server is running...");
+import expres from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = expres();
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
