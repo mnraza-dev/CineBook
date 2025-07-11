c87b61d099ebeb7827f21aefe714602623bf21fc
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 05:11:52 +0530

    restructure Footer and Navbar components for improved layout and styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
+
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5  bg-primary/90">
-      <Link to={"/"}>
-        <img src={assets.logo} alt="Logo" className="w-24 h-auto" />
-      </Link>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/"}>Home</Link>
-    </div>
+    <header className="sticky top-0 left-0 z-50 w-full flex px-6 md:px-16 lg:px-36 py-5 bg-primary/90">
+      <nav className="flex items-center justify-between w-full">
+        <Link to="/">
+          <img src={assets.logo} alt="Logo" className="w-24 h-auto" />
+        </Link>
+        <div className="flex gap-6 items-center text-white font-medium">
+          <Link to="/movies">Movies</Link>
+          <Link to="/my-bookings">My Bookings</Link>
+          <Link to="/favorite">My Favorites</Link>
+        </div>
+      </nav>
+    </header>
   );
 };
 
