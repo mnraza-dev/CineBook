f736a576e1ee9c36920b6fe237fa29a0fc1e6de7
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 05:16:47 +0530

    fix: adjust Navbar layout and styling for better responsiveness

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
-
+import { MenuIcon } from "lucide-react";
 const Navbar = () => {
   return (
     <header className="sticky top-0 left-0 z-50 w-full flex px-6 md:px-16 lg:px-36 py-5 bg-primary/90">
       <nav className="flex items-center justify-between w-full">
-        <Link to="/">
+        <Link className="max-md:flex-1" to="/">
           <img src={assets.logo} alt="Logo" className="w-24 h-auto" />
         </Link>
         <div className="flex gap-6 items-center text-white font-medium">
@@ -14,6 +14,13 @@ const Navbar = () => {
           <Link to="/my-bookings">My Bookings</Link>
           <Link to="/favorite">My Favorites</Link>
         </div>
+        <div>
+          <Link to="/login" className="text-white">
+            Login
+          </Link>
+        </div>
+
+        <MenuIcon className="max-md:ml-4 w-8 h-8 cursor-pointer md:hidden" />
       </nav>
     </header>
   );
