975f6778010cf04cc3fcf677fe7071a9bb1bb842
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 18:10:14 +0530

    feat: enhance Movies page layout with new title section and improved styling

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "lucide-react";
 import MovieCard from "../components/MovieCard";
 const movies = [
   {
@@ -139,10 +140,15 @@ const movies = [
 
 const Movies = () => {
   return (
-    <div className="px-6 md:px-16 lg:px-24 xl:px-44 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+    <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
+        <p className="text-logo-gradient text-3xl font-semibold">New in Cinemas</p>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+     
       {movies.map((movie) => (
         <MovieCard key={movie._id} movie={movie} />
       ))}
+    </div>
     </div>
   );
 };
