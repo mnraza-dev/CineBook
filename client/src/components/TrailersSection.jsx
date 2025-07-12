da2e34dc03685a8e81dcad01259b1a6ef79a2d89
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 20:24:06 +0530

    feat: update TrailersSection to use the second trailer by default; refine layout styles in index.css

--- client/src/components/TrailersSection.jsx
+++ client/src/components/TrailersSection.jsx
@@ -3,24 +3,26 @@ import { dummyTrailers } from "../assets/assets";
 import ReactPlayer from "react-player";
 import BlurCircle from "./BlurCircle";
 import TitleSection from "./TitleSection";
+
 const TrailersSection = () => {
-  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[0]);
+  const [currentTrailer, setCurrentTrailer] = useState(dummyTrailers[1]);
+
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden py-20">
-     <TitleSection
-        title="Trailers"
-        buttonText="See All"
-        to="/trailers"
-      />
-      <div className="relative  mt-6 ">
+      <TitleSection title="Trailers" buttonText="See All" to="/trailers" />
+      <div className="relative mt-6">
         <BlurCircle top="-100px" right="-20px" />
-        <ReactPlayer
-          url={currentTrailer.videoUrl}
-          controls={false}
-          className="mx-auto max-w-full "
-          width={"960px"}
-          height={"540px"}
-        />
+
+          <ReactPlayer
+            url={currentTrailer.videoUrl}
+      
+            controls={false}
+ 
+            className=" mx-auto max-w-full"
+            width="960px"
+            height="540px"
+          />
+        
       </div>
     </div>
   );
