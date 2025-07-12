cc5ef3ffdd652fcaba4ed5086a1c46240f194394
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 20:48:41 +0530

    feat: update TitleSection to allow dynamic button text; replace static title in Movies page

--- client/src/components/TitleSection.jsx
+++ client/src/components/TitleSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
-const TitleSection = ({ title, buttonText = "View All", to = "/movies" }) => {
+const TitleSection = ({ title, buttonText = "", to = "/movies" }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -13,13 +13,15 @@ const TitleSection = ({ title, buttonText = "View All", to = "/movies" }) => {
   return (
     <div className="flex items-center justify-between mb-8 mt-6">
       <p className="text-logo-gradient text-3xl font-semibold">{title}</p>
-      <button
+      {buttonText && (
+        <button
         onClick={handleClick}
         className="login-gradient-diagonal hover:bg-[#f5ec9b] border-2 border-[#9d9974] hover:border-[#f5ec9b] text-black px-6 py-2 text-sm rounded-full flex items-center gap-2 hover:opacity-90 transition font-medium cursor-pointer"
       >
         {buttonText}
         <ArrowRight className="inline-block ml-2 w-4 h-4" />
       </button>
+      )}
     </div>
   );
 };
--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,12 +1,13 @@
 import MovieCard from "../components/MovieCard";
+import TitleSection from "../components/TitleSection";
 import { movies } from "../data/movies";
-
+import BlurCircle from "../components/BlurCircle";
 const Movies = () => {
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
-      <p className="text-logo-gradient text-3xl font-semibold">
-        New in Cinemas
-      </p>
+      <BlurCircle top="150px" left="0" />
+      <BlurCircle bottom="50px" right="50px" />
+      <TitleSection title="Now Showing" />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
         {movies.map((movie) => (
           <MovieCard key={movie._id} movie={movie} />
