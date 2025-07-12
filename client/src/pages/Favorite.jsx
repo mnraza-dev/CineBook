2be5ed147170b55f5b5c398d3f2e09d79ad27ad6
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 21:09:01 +0530

    feat: implement Favorite page layout with movie display and no movies message

--- client/src/pages/Favorite.jsx
+++ client/src/pages/Favorite.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import MovieCard from "../components/MovieCard";
+import TitleSection from "../components/TitleSection";
+import { movies } from "../data/movies";
+import BlurCircle from "../components/BlurCircle";
 const Favorite = () => {
   return (
-    <div>Favorite</div>
-  )
-}
+    <div className="px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden">
+      <BlurCircle bottom="0px" right="50px" />
+      {movies.length > 0 && <TitleSection title="Your Favorite Movies" />}
+      <BlurCircle top="150px" left="0" />
+      {movies.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-10">
+          {movies.map((movie) => (
+            <MovieCard key={movie._id} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-screen flex-col">
+          <h1 className="text-3xl font-bold text-center">
+            No Movies Available
+          </h1>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Favorite
+export default Favorite;
