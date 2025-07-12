9dd9dd7bdaebb8ccbc0a01f69b2a2695510e9f54
Md Noorullah Raza <mnraza94@gmail.com>
2025-07-12 20:36:13 +0530

    feat: enhance Footer component with improved layout and additional content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,14 +3,64 @@ import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className=" z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5 ">
-      <Link to={"/"}>
-        <img src={assets.logo} alt="Logo" className="w-24 h-auto" />
-      </Link>
-      <h1 className="text-logo-gradient text-4xl font-bold">
-          Footer Goes Here
-        </h1>
-    </div>
+    <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+        <div className="md:max-w-96">
+          <Link to={"/"}>
+            <img src={assets.logo} alt="Logo" className="h-auto h-11 w-24 " />
+          </Link>
+
+          <p className="mt-6 text-sm">
+            CineBook is a modern and responsive movie booking application built
+            using React and TailwindCSS, designed to deliver a cinematic
+            experience right from your screen. Users can browse trending movies,
+            watch official trailers, explore genres, and conveniently book
+            tickets — all from a seamless, intuitive interface.
+          </p>
+          <div className="flex items-center gap-2 mt-4">
+            <img
+              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/googlePlayBtnBlack.svg"
+              alt="google play"
+              className="h-10 w-auto border border-white rounded"
+            />
+            <img
+              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/appleStoreBtnBlack.svg"
+              alt="app store"
+              className="h-10 w-auto border border-white rounded"
+            />
+          </div>
+        </div>
+        <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
+          <div>
+            <h2 className="font-semibold mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About us</a>
+              </li>
+              <li>
+                <a href="#">Contact us</a>
+              </li>
+              <li>
+                <a href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-semibold mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+1-234-567-890</p>
+              <p>contact@example.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="pt-4 text-center text-sm pb-5">
+        Copyright {new Date().getFullYear()} © PreBuiltUI. All Right Reserved.
+      </p>
+    </footer>
   );
 };
 
